da553c3ff0a2b48496df5f557fae55c569769350
hong19 <jimmy600kimo@hotmail.com>
2016-03-12 18:06:57 +0800

    feat(InsertStores): implement insertstores

---
+++ app/Console/Commands/InsertStores.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Store;
+use Illuminate\Console\Command;
+
+class InsertStores extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:insert_stores {filePath}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'insert stores into database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->line('insert stores start');
+
+        $filePath = $this->argument('filePath');
+        $fileContent = file($filePath);
+
+        //dd($fileContent);
+        for ($i = 1 ; $i < sizeof($fileContent) ; $i++) {
+            $values = explode(',', $fileContent[$i]);
+
+            Store::create([
+                'name' => $values[0],
+                'phone' => $values[2],
+                'address' => $values[3]
+            ]);
+
+        }
+
+        $this->line('insert stores complete');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         // Commands\Inspire::class,
-        Commands\InsertFoods::class
+        Commands\InsertFoods::class,
+        Commands\InsertStores::class
     ];
 
     /**
