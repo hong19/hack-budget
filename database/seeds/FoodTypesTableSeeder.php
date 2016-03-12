f92a6bad1ba651fc92403abe354560546bde6cdd
hong19 <jimmy600kimo@hotmail.com>
2016-03-12 18:15:36 +0800

    feat(Seeder): add food types

---
+++ database/seeds/FoodTypesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\FoodType;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FoodTypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('set foreign_key_checks=0');
+        FoodType::truncate();
+        DB::statement('set foreign_key_checks=1');
+
+        FoodType::create([
+            'id' => 1,
+            'food_type_name' => 'staple'
+        ]);
+
+        FoodType::create([
+            'id' => 2,
+            'food_type_name' => 'snack'
+        ]);
+
+        FoodType::create([
+            'id' => 3,
+            'food_type_name' => 'drink'
+        ]);
+    }
+
+}
